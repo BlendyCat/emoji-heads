fbf30d2e7e728d92d4114be9c06a7ae57eabb5e4
evan.merz <bananamerz@yahoo.com>
2016-12-10 22:10:27 -0800

    Added plugin main class

---
+++ src/main/java/com/naturaliscraft/emojihead/EmojiHeads.java
@@ -0,0 +1,20 @@
+package com.naturaliscraft.emojihead;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by EvanMerz on 12/10/16.
+ */
+
+public class EmojiHeads extends JavaPlugin{
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
